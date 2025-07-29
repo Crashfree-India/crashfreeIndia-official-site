1b0bc8fd439c16360b528c6cfb085b53a15aeab5
shradha mantry <itsmehak27@gmail.com>
2025-07-29 15:23:17 +0530

    blog-section-updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 // tailwind.config.js
+import typography from '@tailwindcss/typography'
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+  ], safelist: [
+    'text-orange-500',
+    'text-blue-500',
+    'text-green-600',
+    'text-red-500',
+    'text-yellow-500',
+    'text-purple-500',
+    'bg-orange-100',
+    'bg-blue-100',
+    'bg-green-100',
+    'bg-red-100',
+    'bg-yellow-100',
+    'bg-purple-100',
+    'bg-orange-50',
   ],
   theme: {
     extend: {
@@ -14,5 +30,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 }
